e381196a3e0006a72db506e0374ac81e244a5020
Ugo Stephant <ugo@poool.fr>
2020-05-05 14:44:46 +0200

    chore: move babel config to js file

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  'presets': [
+    ['@babel/preset-env', {
+      corejs: 3,
+      useBuiltIns: 'usage',
+    }],
+    '@babel/preset-react',
+  ],
+  'plugins': [
+    ['@babel/plugin-transform-runtime', {
+      corejs: 3,
+    }],
+    '@babel/plugin-proposal-class-properties',
+  ],
+};
